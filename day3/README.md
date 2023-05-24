28d06dde347c58a7fb622e7b675bad550dce7b32
David Garvie <david.garvie@omnipresent.com>
2023-05-24 16:48:01 +0100

    complete day 3 challenge

---
+++ day3/README.md
@@ -0,0 +1,24 @@
+# Day 3
+
+Mostly learning about conditionals today and the importance of spacing in Python. A simple if block looks like this: 
+
+```py
+if True:
+  print("The universe is aligned")
+else:
+  print("We are all doomed")
+```
+
+Note, the print statement MUST be nested within the parent `if` statement.
+
+## Operators
+- `and` - checks ALL conditions are true (e.g. `a > 5 and a < 10`) 
+- `or` - checks if ANY conditions are true (e.g. `a > 5 or a < 10` )
+- `not` checks if NO conditions are true (e.g. `not a > 5`)
+
+## Functions
+- `count` - will return the number of characters that match a string. e.g. `"hello".count("l")` returns 2
+- `lower` will convert a string to lowercase. e.g. `"Hello".lower()` returns "hello"
+- `quit` allows us to exit the script early
+
+[Fun ascii art](https://ascii.co.uk/art) used in treasure island game
---
+++ day3/main.py
@@ -0,0 +1,64 @@
+print(
+    """
+  ____________________________________________________________________
+ / \-----     ---------  -----------     -------------- ------    ----\\
+ \_/__________________________________________________________________/
+ |~ ~~ ~~~ ~ ~ ~~~ ~ _____.----------._ ~~~  ~~~~ ~~   ~~  ~~~~~ ~~~~|
+ |  _   ~~ ~~ __,---'_       "         `. ~~~ _,--.  ~~~~ __,---.  ~~|
+ | | \___ ~~ /      ( )   "          "   `-.,' (') \~~ ~ (  / _\ \~~ |
+ |  \    \__/_   __(( _)_      (    "   "     (_\_) \___~ `-.___,'  ~|
+ |~~ \     (  )_(__)_|( ))  "   ))          "   |    "  \ ~~ ~~~ _ ~~|
+ |  ~ \__ (( _( (  ))  ) _)    ((     \\//    " |   "    \_____,' | ~|
+ |~~ ~   \  ( ))(_)(_)_)|  "    ))    //\\ " __,---._  "  "   "  /~~~|
+ |    ~~~ |(_ _)| | |   |   "  (   "      ,-'~~~ ~~~ `-.   ___  /~ ~ |
+ | ~~     |  |  |   |   _,--- ,--. _  "  (~~  ~~~~  ~~~ ) /___\ \~~ ~|
+ |  ~ ~~ /   |      _,----._,'`--'\.`-._  `._~~_~__~_,-'  |H__|  \ ~~|
+ |~~    / "     _,-' / `\ ,' / _'  \`.---.._          __        " \~ |
+ | ~~~ / /   .-' , / ' _,'_  -  _ '- _`._ `.`-._    _/- `--.   " " \~|
+ |  ~ / / _-- `---,~.-' __   --  _,---.  `-._   _,-'- / ` \ \_   " |~|
+ | ~ | | -- _    /~/  `-_- _  _,' '  \ \_`-._,-'  / --   \  - \_   / |
+ |~~ | \ -      /~~| "     ,-'_ /-  `_ ._`._`-...._____...._,--'  /~~|
+ | ~~\  \_ /   /~~/    ___  `---  ---  - - ' ,--.     ___        |~ ~|
+ |~   \      ,'~~|  " (o o)   "         " " |~~~ \_,-' ~ `.     ,'~~ |
+ | ~~ ~|__,-'~~~~~\    \"/      "  "   "    /~ ~~   O ~ ~~`-.__/~ ~~~|
+ |~~~ ~~~  ~~~~~~~~`.______________________/ ~~~    |   ~~~ ~~ ~ ~~~~|
+ |____~jrei~__~_______~~_~____~~_____~~___~_~~___~\_|_/ ~_____~___~__|
+ / \----- ----- ------------  ------- ----- -------  --------  -------\\
+ \_/__________________________________________________________________/
+"""
+)
+
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+response_1 = input(
+    "There is a fork in the path ahead of you. Do you turn left or right? "
+).lower()
+
+if not response_1 == "left":
+    print("ARGH!!! You fall off the path and into a deep dark hole")
+    print("GAME OVER")
+    quit()
+
+response_2 = input("You stumble into a lagoon. Do you swim through it? ").lower()
+
+if response_2 == "yes":
+    print("ARGH!!! You are brutally mauled by a crocodile")
+    print("GAME OVER")
+    quit()
+
+print("Beyond the lagoon you see something in the distance.")
+print("There are three doors to chose from: Red, Yellow or Blue.")
+response_3 = input("Which door do you choose? ").lower()
+
+if response_3 == "red":
+    print("ARGH!!! You are instantly burned alive by a huge fireball")
+    print("GAME OVER")
+elif response_3 == "blue":
+    print("ARGH!!! You are devoured by a hoard of beats")
+    print("GAME OVER")
+elif response_3 == "yellow":
+    print("You chose wisely, congratulations")
+    print("YOU WIN!")
+else:
+    print("GAME OVER")
